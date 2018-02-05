cd4c5882e74f1909bddd6a4ceb9384e58b533861
marcosumali <marco.sumali90@gmail.com>
2018-02-05 20:02:59 +0700

    submit hasil recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,61 @@
-function to_roman(input) {
+function to_roman(num) {
   // start your code here
-  return to_roman(input);
+
+  var hasilRoman = '';
+
+  var arrRoman = [
+    { angka: 1,
+      roman: 'I'},
+    { angka: 4,
+      roman: 'IV'},
+    { angka: 5,
+      roman: 'V'},
+    { angka: 9,
+      roman: 'IX'},
+    { angka: 10,
+      roman: 'X'},
+    { angka: 40,
+      roman: 'XL'},
+    { angka: 50,
+      roman: 'L'},
+    { angka: 90,
+      roman: 'XC'},
+    { angka: 100,
+      roman: 'C'},
+    { angka: 400,
+      roman: 'CD'},
+    { angka: 500,
+      roman: 'D'},
+    { angka: 900,
+      roman: 'CM'},
+    { angka: 1000,
+      roman: 'M'}
+  ]
+
+  // console.log(angkaMax); // 100
+  // console.log(indexMax); // 8
+  // console.log(arrRoman[indexMax].roman); // C
+
+  if (num == 0) {
+    // base case
+    return '';
+  } else {
+    // recursion case
+    var angkaMax = arrRoman[0].angka; // 
+    var indexMax = 0;
+    for (var i = 0; i < arrRoman.length; i++) { // 13 // untuk cari angka terdekat
+      if (arrRoman[i].angka <= num && arrRoman[i].angka > angkaMax) {
+        angkaMax = arrRoman[i].angka;
+        indexMax++;
+      }
+    }
+
+    num -= arrRoman[indexMax].angka;
+
+    return arrRoman[indexMax].roman + to_roman(num);
+  }
+
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
