3923b41c0f8addd681cab07234e3ba143b91f505
Agung Prabowo <33645449+agungcaproex@users.noreply.github.com>
2018-02-05 18:58:41 +0700

    upload roman-numerals-recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input);
+  var arabic = input;
+  var roman = [["M",1000],["D",500],["CD",400],["C",100],["L",50],["XL",40],["X",10],["IX",9],["V",5],["IV",4],["I",1]];
+  var hasil = "";
+  
+  if(input > 0){
+	  for(var i=0; i<roman.length; i++){
+		  if(arabic >= roman[i][1]){
+			  hasil += roman[i][0];
+			  return hasil += to_roman(arabic - roman[i][1]);
+		  }
+	  }
+  }
+  
+  
+  return hasil;
 }
 
 console.log('My totally sweet testing script for new roman\n')
