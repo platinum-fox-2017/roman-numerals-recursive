098a64ddce46dafe3d2a5bf603ca933d91673da3
unknown <fatimah.azzhr@gmail.com>
2018-02-05 23:36:24 +0700

    num to rom recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,29 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input);
+  var roman = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+  ];
+
+  if (input === 0) {
+    return "";
+  }
+
+  for (i = 0; i < roman.length; i++) {
+    if (input >= roman[i][0]) {
+      return roman[i][1] + to_roman(input - roman[i][0]);
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
