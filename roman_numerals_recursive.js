3ed992131e8145431d7d487e22c95c6d088e85d5
ryanponti <ryanakbar.armial@gmail.com>
2018-02-05 11:10:25 +0700

    Template

---
+++ roman_numerals_recursive.js
@@ -0,0 +1,13 @@
+function to_roman(input) {
+  // start your code here
+  return to_roman(input);
+}
+
+console.log('My totally sweet testing script for new roman\n')
+console.log('input | expected | actual')
+console.log('——————|——————————|————————')
+console.log('4     | IV       | ', to_roman(4))
+console.log('9     | IX       | ', to_roman(9))
+console.log('23    | XXIII    | ', to_roman(23))
+console.log('1453  | MCDLIII  | ', to_roman(1453))
+console.log('1646  | MDCXLVI  | ', to_roman(1646))
