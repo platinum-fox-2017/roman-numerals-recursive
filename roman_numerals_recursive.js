35d39432f10c4ed602d7513478b344f5da750a06
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-05 16:30:47 +0700

    release 0

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,13 +1,25 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input);
+function to_roman(num) {
+  // your implementation code here
+  let romanNumeral = ['M', 'MC', 'D', 'DC', 'C', 'CX', 'L', 'LX', 'X', 'IX', 'V', 'IV', 'I']
+  let arabicNumeral = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  if (num <= 0) {
+    return ""
+  } else {
+    for (let [index, value] of arabicNumeral.entries()) {
+      console.log(index, value)
+      if (num >= value) {
+        return romanNumeral[index] + to_roman(num - value)
+      }
+    }
+  }
 }
 
+// Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
-console.log('——————|——————————|————————')
+console.log('——————|——————————|———————')
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
-console.log('23    | XXIII    | ', to_roman(23))
+console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
