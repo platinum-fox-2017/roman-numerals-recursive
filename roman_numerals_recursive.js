fc5545e6b4f661bb53090c7b4635e68183a876f6
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-05 20:55:29 +0700

    updated

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,13 +1,43 @@
+//Solution 1
 function to_roman(input) {
-  var romanArray = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
-  var numberArray = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-  if(input==0){
+  var romanArray = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+  var numberArray = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  if (input == 0) {
     return "";
   }
 
-  for(var i =numberArray.length-1; i>=0; i--){
-    if(input/numberArray[i]>=1){
-      return romanArray[i]+to_roman(input-=numberArray[i])
+  for (var i = numberArray.length - 1; i >= 0; i--) {
+    if (input / numberArray[i] >= 1) {
+      return romanArray[i] + to_roman(input -= numberArray[i])
+    }
+  }
+}
+
+//Solution 2
+function to_roman(input) {
+  var romanObject = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  if (input == 0) {
+    return "";
+  }
+
+  for (var i in romanObject) {
+    if (input / romanObject[i] >= 1) {
+      return i + to_roman(input -= romanObject[i])
     }
   }
 }
