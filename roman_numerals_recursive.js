b0f83e9807f5876a007f64408d59c68d9839244c
root <irsyadpahlapi28@gmail.com>
2018-02-05 04:52:47 -0500

    upload fil js roman numeral rekursif

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,21 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input);
+  var number=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  var romawi=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+  if(input<1){
+    return ''
+  }
+  else{
+    var simpan=''
+    for(let i=0;i<number.length;i++){
+      if(input>=number[i]){
+        simpan += romawi[i]
+        input=input-number[i]
+        break;
+      }
+    }
+    return simpan+to_roman(input);
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
