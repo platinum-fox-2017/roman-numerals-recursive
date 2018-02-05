bb633fe302e9edca6249c35607aec962a11687d8
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-05 22:22:28 +0700

    updated

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,13 +1,82 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input);
+function toRoman(num){
+  // let kamusRoman = ['M','C','D','DC', 'C', 'CX', 'L', 'LX', 'X', 'IX', 'V', 'IV', 'I']
+  // let ejaanArabic = [1000,900,500,400,100,90,50,40,10, 9, 5,4,1]
+  let nilais = [{
+      romanNumeral : "M",
+      digits : 1000,
+  },
+  {
+      romanNumeral : "C",
+      digits : 900,
+  },
+  {
+      romanNumeral : "D",
+      digits : 500,
+  },
+  {
+      romanNumeral : "CD",
+      digits : 400,
+  },
+  {
+      romanNumeral : "C",
+      digits : 100,
+  },
+  {
+      romanNumeral : "CX",
+      digits : 90,
+  },
+  {
+      romanNumeral : "L",
+      digits : 50,
+  },
+  {
+      romanNumeral : "XL",
+      digits : 40,
+  },
+  {
+      romanNumeral : "X",
+      digits : 10,
+  },
+  {
+      romanNumeral : "XI",
+      digits : 9,
+  },
+  {
+      romanNumeral : "V",
+      digits : 5,
+  },
+  {
+      romanNumeral : "IV",
+      digits : 4,
+  },
+  {
+      romanNumeral : "I",
+      digits : 1,
+  }]
+  let hasil = ""
+  if(num <= 0){
+      return ""
+  }else{
+      // for (let [index, value] of ejaanArabic.entries()) {
+      //     if(num / value >=1){
+      //         return kamusRoman[index] + toRoman(num - value)
+      //     }
+      // }
+
+      const result = nilais.filter(nilai =>{
+          let hasilBagi = num / nilai.digits
+          if(hasilBagi >= 1){
+              return nilai
+          }
+      })
+
+      return result[0].romanNumeral + toRoman(num - result[0].digits)
+  }
+  
 }
 
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('23    | XXIII    | ', to_roman(23))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log(toRoman(4))
+console.log(toRoman(9))
+console.log(toRoman(23))
+console.log(toRoman(1453))
+console.log(toRoman(1646))
