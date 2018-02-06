3dc60611c30951e64e768b3edd61fc35249d0910
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-06 14:32:59 +0700

    fix typo

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,12 +1,13 @@
+
 function to_roman(num) {
   // your implementation code here
-  let romanNumeral = ['M', 'MC', 'D', 'DC', 'C', 'CX', 'L', 'LX', 'X', 'IX', 'V', 'IV', 'I']
+  let romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
   let arabicNumeral = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
   if (num <= 0) {
     return ""
   } else {
     for (let [index, value] of arabicNumeral.entries()) {
-      console.log(index, value)
+      // console.log(index, value)
       if (num >= value) {
         return romanNumeral[index] + to_roman(num - value)
       }
